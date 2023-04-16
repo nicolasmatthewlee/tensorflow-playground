f882458330b19111a3d331d6dc7ec2b9da59b8a4
nicolasmatthewlee <nicolasmatthewlee@gmail.com>
2023-04-16 16:11:42 -0700

    setup model on server to return prediction given data received: 1. updated curl to send 28x28 matrix from x_test[0], 2. load model and return predicted number

--- server.py
+++ server.py
@@ -1,5 +1,16 @@
 import flask
 from flask import Flask
+import tensorflow as tf
+import numpy as np
+
+# load model
+model = tf.keras.models.load_model('mnist-model')
+
+
+def solve_predictions(predictions):
+    'converts output of model.predict to the number with the highest activation'
+    return list(map(lambda x: int(np.where(np.isclose(x, max(x)) == True)[0][0]), predictions))
+
 
 # 1. create application instance
 app = Flask(__name__)
@@ -15,5 +26,5 @@ def index():
 @app.post('/api/model/predict')
 def api_model_predict():
     data = flask.request.get_json()
-    print(data)
-    return flask.jsonify(data)
+    prediction = solve_predictions(model.predict([data]))
+    return flask.jsonify(prediction[0])
